8fe0a7b934916bf04c7744d63d1c959e20b84d09
missgoin <mister.goindo@gmail.com>
2023-07-07 11:08:16 +0700

    add local2

--- fs/exec.c
+++ fs/exec.c
@@ -1528,8 +1528,9 @@ static int exec_binprm(struct linux_binprm *bprm)
 }
 
 #ifdef CONFIG_KSU
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			void *envp, int *flags);
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags);
 #endif
 
 /*
@@ -1547,7 +1548,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 	int retval;
 	
 	#ifdef CONFIG_KSU
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	if (unlikely(ksu_execveat_hook))
+	   ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+	   ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
 	#endif
 
 	if (IS_ERR(filename))
